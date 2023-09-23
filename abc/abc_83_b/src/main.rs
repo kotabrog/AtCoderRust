238434125f3b6b495febd953459f85a872a2fdb3
kotabrog <suzuki.kota0331@gmail.com>
2023-09-23 15:04:09 +0900

    add abc_83_b

---
+++ abc/abc_83_b/src/main.rs
@@ -0,0 +1,71 @@
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn main() {
+    input! {
+        list: [i32; 3],
+    }
+    let n = list[0];
+    let a = list[1];
+    let b = list[2];
+
+    let mut sum = 0;
+    for i in 0..n {
+        let mut target = i + 1;
+        let mut target_sum = 0;
+        while target > 0 {
+            target_sum += target % 10;
+            target /= 10;
+        }
+        if a <= target_sum && target_sum <= b {
+            sum += i + 1;
+        }
+    }
+    println!("{}", sum);
+}
